2100cfeb352e6ced6513797323c47d7b48618fb5
Saeed <muhammadsaeed033696@gmail.com>
2024-05-23 12:13:08 +0500

    Updated

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,8 +4,42 @@ import Style from "./about.module.css";
 const AboutPage = () => {
   return (
     <>
-      <div className={Style.imgContainer}>
-        <Image src="/about.png" alt="" fill />
+      <div className={Style.container}>
+        <div className={Style.textContainer}>
+          <h2 className={Style.subTitle}>About Agency</h2>
+          <h1 className={Style.title}>
+            We create digital ideas that are bigger, bolder, braver and better.
+          </h1>
+          <p className={Style.desc}>
+            We create digital ideas that are bigger, bolder, braver and better.
+            We believe in good ideas flexibility and precission We’re world’s
+            Our Special Team best consulting & finance solution provider. Wide
+            range of web and software development services.
+          </p>
+
+          <div className={Style.boxes}>
+            <div className={Style.box}>
+              <h1>10 K+</h1>
+              <p>Year of experience</p>
+            </div>
+            <div className={Style.box}>
+              <h1>10 K+</h1>
+              <p>Year of experience</p>
+            </div>
+            <div className={Style.box}>
+              <h1>10 K+</h1>
+              <p>Year of experience</p>
+            </div>
+          </div>
+        </div>
+        <div className={Style.imgContainer}>
+          <Image
+            src="/about.png"
+            alt="About Image"
+            fill
+            className={Style.img}
+          />
+        </div>
       </div>
     </>
   );
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+import Style from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <>
+      <div className={Style.container}>
+        <div className={Style.imgContainer}>
+          <Image src="/contact.png" alt="" fill className={Style.img} />
+        </div>
+        <div className={Style.formContainer}>
+          <form action="" className={Style.form}>
+            <input type="text" placeholder="Name and Surname" />
+            <input type="text" placeholder="Email Address" />
+            <input type="text" placeholder="Phone Number (Optional)" />
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Message"
+            ></textarea>
+            <button>Send</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ContactPage;
